542c256dc16cb8d12da0dbb16931e9cdf9fd996d
Shannon Robinson <Srobinson@wurllink.com>
2020-05-14 16:45:28 -0400

    Create Waivers enum

---
+++ Jamwest Reservation/Model/WaiverSelection.swift
@@ -0,0 +1,15 @@
+//
+//  WaiverSelection.swift
+//  Jamwest Reservation
+//
+//  Created by Shannon Robinson on 5/14/20.
+//  Copyright © 2020 Wurllink. All rights reserved.
+//
+
+import UIKit
+
+// enum
+enum Waivers {
+    case PendingWaivers
+    case ApprovedWaivers
+}
