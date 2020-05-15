393abca04c730777bc34edc491002d297062771f
Shannon Robinson <Srobinson@wurllink.com>
2020-05-15 11:31:42 -0400

    Modify ApprovedWaiver model to inherit from WaiverVerification model

---
+++ Jamwest Reservation/Model/Verification Flow/ApprovedWaiver.swift
@@ -0,0 +1,17 @@
+//
+//  ApprovedWaiver.swift
+//  Jamwest Reservation
+//
+//  Created by Shannon Robinson on 5/14/20.
+//  Copyright © 2020 Wurllink. All rights reserved.
+//
+
+import Foundation
+
+class ApprovedWaiver: WaiverVerification{
+    
+    override init(waiverID: String!, dictionary: Dictionary<String, AnyObject>) {
+        
+        super.init(waiverID: waiverID, dictionary: dictionary)
+    }
+}
