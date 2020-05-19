3555aba8642eaa4f513c1516c24dd7eb5d11f062
Shannon Robinson <Srobinson@wurllink.com>
2020-05-19 14:02:47 -0400

    Create EditReservation custom cell Configure EditReservationCell

---
+++ Jamwest Reservation/View/EditReservationCell.swift
@@ -0,0 +1,53 @@
+//
+//  EditReservationCell.swift
+//  Jamwest Reservation
+//
+//  Created by Shannon Robinson on 5/19/20.
+//  Copyright © 2020 Wurllink. All rights reserved.
+//
+
+import UIKit
+
+class EditReservationCell: UITableViewCell {
+
+    var reservation : EditReservation? {
+        
+        didSet {
+            
+            guard let name = reservation?.group,
+                  let hotel = reservation?.hotel else { return }
+            
+            textLabel?.text = name
+            detailTextLabel?.text = hotel
+        }
+    }
+    
+    //    MARK: - Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        backgroundColor = .clear
+        
+        // separator insets
+        separatorInset = UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 40)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(x: 40, y: (textLabel?.frame.origin.y)! - 2, width: (frame.width) / 2, height: ((textLabel?.frame.height)!))
+        textLabel?.textColor = .black
+        
+        textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        
+        detailTextLabel?.frame = CGRect(x: 40, y: (detailTextLabel?.frame.origin.y)!, width: (frame.width) / 2, height: (detailTextLabel?.frame.height)! + 2)
+        
+        detailTextLabel?.textColor = .lightGray
+        detailTextLabel?.font = UIFont.systemFont(ofSize: 16)
+
+    }
+}
