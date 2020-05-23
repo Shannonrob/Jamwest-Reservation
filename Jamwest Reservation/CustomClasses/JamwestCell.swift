48942ee3e7f509159a6fdc978bc575fc235a04ad
Shannon Robinson <Srobinson@wurllink.com>
2020-05-23 18:03:24 -0400

    Create JamwestCustom tableViewCell

---
+++ Jamwest Reservation/CustomClasses/JamwestCell.swift
@@ -0,0 +1,71 @@
+//
+//  JamwestCell.swift
+//  Jamwest Reservation
+//
+//  Created by Shannon Robinson on 5/23/20.
+//  Copyright © 2020 Wurllink. All rights reserved.
+//
+
+import UIKit
+
+class JamwestCell: UITableViewCell {
+
+    //    MARK: - Init
+    override init(style: UITableViewCell.CellStyle , reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+
+        configureCell()
+        
+        headerLabel.text = "Header"
+        detailLabel.text = "Detail"
+        backgroundColor = .clear
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//    MARK: - Properties
+    
+    let cellView: UIView = {
+        
+        let view = UIView()
+        view.setShadow()
+        view.layer.cornerRadius = 4
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    
+//    MARK: - Labels
+    
+    let headerLabel: UILabel = {
+        
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 18)
+        return label
+    }()
+    
+    let detailLabel: UILabel = {
+        
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 18)
+        return label
+    }()
+    
+    
+    func configureCell() {
+        
+        let labelStackView = UIStackView(arrangedSubviews: [headerLabel, detailLabel])
+        labelStackView.configureStackView(alignment: .leading, distribution: .fillEqually, spacing: 0)
+        labelStackView.axis = .vertical
+
+        addSubview(cellView)
+        cellView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 4, paddingLeft: 35, paddingBottom: 4, paddingRight: 35, width: 0, height: 0)
+        
+        addSubview(labelStackView)
+        labelStackView.anchor(top: cellView.topAnchor, left: cellView.leftAnchor, bottom: cellView.bottomAnchor, right: nil, paddingTop: 6, paddingLeft: 15, paddingBottom: 6, paddingRight: 0, width: 0, height: 0)
+    }
+}
