853bd23cde5e82c1fedec277240cf57593bb9088
Shannon Robinson <Srobinson@wurllink.com>
2020-05-23 18:05:29 -0400

    Modify ApprovedWaiverCell to inherit from JamwestCell

--- Jamwest Reservation/View/Verification Flow/ApprovedWaiverCell.swift
+++ Jamwest Reservation/View/Verification Flow/ApprovedWaiverCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ApprovedWaiverCell: UITableViewCell {
+class ApprovedWaiverCell: JamwestCell {
     
     var approvedWaiver: ApprovedWaiver? {
         
@@ -18,35 +18,8 @@ class ApprovedWaiverCell: UITableViewCell {
 //            guard let imageURL = approvedWaiver?.imageURL else { return }
             guard let date = approvedWaiver?.creationDate else { return}
             
-            textLabel?.text = name
-            detailTextLabel?.text = date
+            headerLabel.text = name
+            detailLabel.text = date
         }
-    }
-    
-    //    MARK: - Init
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-        
-        backgroundColor = .clear
-        textLabel?.text = "Shannon Robinson"
-        detailTextLabel?.text = "May 13, 2020"
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        textLabel?.frame = CGRect(x: 40, y: (textLabel?.frame.origin.y)! - 2, width: (frame.width) / 2, height: ((textLabel?.frame.height)!))
-        textLabel?.textColor = .black
-        
-        textLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        
-        detailTextLabel?.frame = CGRect(x: 40, y: (detailTextLabel?.frame.origin.y)!, width: (frame.width) / 2, height: (detailTextLabel?.frame.height)! + 5)
-        
-        detailTextLabel?.textColor = .lightGray
-        detailTextLabel?.font = UIFont.systemFont(ofSize: 18)
     }
 }
