ce0129daf3ca762a3946f7384e28c1c22bc12ccd
Shannon Robinson <Srobinson@wurllink.com>
2020-05-23 18:04:51 -0400

    Modify EmailListCell to inherit from JamwestCell

--- Jamwest Reservation/View/EmailListCell.swift
+++ Jamwest Reservation/View/EmailListCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class EmailListCell: UITableViewCell {
+class EmailListCell: JamwestCell {
     
     var emailList : EmailList? {
         
@@ -17,34 +17,8 @@ class EmailListCell: UITableViewCell {
             guard let name = emailList?.name,
                 let email = emailList?.emailAddress else { return }
             
-            textLabel?.text = name
-            detailTextLabel?.text = email
+            headerLabel.text = name
+            detailLabel.text = email
         }
-    }
-    
-    //    MARK: - Init
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-        
-        backgroundColor = .clear
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        textLabel?.frame = CGRect(x: 40, y: (textLabel?.frame.origin.y)! , width: (frame.width) / 2, height: ((textLabel?.frame.height)!))
-        textLabel?.textColor = .black
-        
-        textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        
-        detailTextLabel?.frame = CGRect(x: 40, y: (detailTextLabel?.frame.origin.y)!, width: (frame.width) / 2, height: (detailTextLabel?.frame.height)! + 2)
-        
-        detailTextLabel?.textColor = .lightGray
-        detailTextLabel?.font = UIFont.systemFont(ofSize: 16)
-        
     }
 }
