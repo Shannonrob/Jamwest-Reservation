b6cef74571011b3999033a9902be2f066c36fe1d
Shannon Robinson <Srobinson@wurllink.com>
2020-05-23 18:04:14 -0400

    Modify editReservationCell to inherit from JamwestCell

--- Jamwest Reservation/View/EditReservationCell.swift
+++ Jamwest Reservation/View/EditReservationCell.swift
@@ -8,43 +8,17 @@
 
 import UIKit
 
-class EditReservationCell: UITableViewCell {
-
+class EditReservationCell: JamwestCell {
+    
     var reservation : EditReservation? {
         
         didSet {
             
             guard let name = reservation?.group,
-                  let hotel = reservation?.hotel else { return }
+                let hotel = reservation?.hotel else { return }
             
-            textLabel?.text = name
-            detailTextLabel?.text = hotel
+            headerLabel.text = name
+            detailLabel.text = hotel
         }
-    }
-    
-    //    MARK: - Init
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
-        
-        backgroundColor = .clear
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        textLabel?.frame = CGRect(x: 40, y: (textLabel?.frame.origin.y)! , width: (frame.width) / 2, height: ((textLabel?.frame.height)!))
-        textLabel?.textColor = .black
-        
-        textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        
-        detailTextLabel?.frame = CGRect(x: 40, y: (detailTextLabel?.frame.origin.y)!, width: (frame.width) / 2, height: (detailTextLabel?.frame.height)! + 2)
-        
-        detailTextLabel?.textColor = .lightGray
-        detailTextLabel?.font = UIFont.systemFont(ofSize: 16)
-
     }
 }
